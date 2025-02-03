3d4f64089ccdbe6e0392729829d934719bbe1cde
SerhiiKI13 <kugai.sirozha@gmail.com>
2025-02-03 08:19:20 +0100

    start learn javascript

--- my/mainy.js
+++ my/mainy.js
@@ -47,30 +47,70 @@
 
 
 //
+//function exchange(sumUAH,currencyValues,exchangeCurrency){
+//     let exchangeRate = 0;
+//     for (let i = 0; i < currencyValues.length; i++){
+//         if (currencyValues[i].currency === exchangeCurrency){
+//             exchangeRate = currencyValues[i].value;
+//         }
+//     }
+//
+//     if (exchangeRate > 0){
+//         return sumUAH / exchangeRate;
+//     } else {
+//         return "Валюта не знайдена";
+//     }
+// }
+// const currencies = [
+//     { currency: 'USD', value: 41.76 },
+//     { currency: 'EUR', value: 43.27 },
+//     { currency: 'GPB', value: 51.75 }
+// ];
+//
+// console.log(exchange(10000, currencies, 'USD')); // -> 239.5
+// console.log(exchange(10000, currencies, 'EUR')); // -> 231.1
+// console.log(exchange(10000, currencies, 'GPB')); // -> 193.2
+// console.log(exchange(10000, currencies, 'JPY')); // -> "Валюта не знайдена"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//- Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+// Приклад exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD') // => 400
 function exchange(sumUAH,currencyValues,exchangeCurrency){
-    let exchangeRate = 0;
-    for (let i = 0; i < currencyValues.length; i++){
+   let exchangeRate = 0;
+    for (let i = 0;i < currencyValues.length; i++){
         if (currencyValues[i].currency === exchangeCurrency){
             exchangeRate = currencyValues[i].value;
         }
     }
-
     if (exchangeRate > 0){
         return sumUAH / exchangeRate;
     } else {
-        return "Валюта не знайдена";
+        return 'Валюты для обмена не найдено';
     }
 }
-const currencies = [
-    { currency: 'USD', value: 41.76 },
-    { currency: 'EUR', value: 43.27 },
-    { currency: 'GPB', value: 51.75 }
-];
-
-console.log(exchange(10000, currencies, 'USD')); // -> 239.5
-console.log(exchange(10000, currencies, 'EUR')); // -> 231.1
-console.log(exchange(10000, currencies, 'GPB')); // -> 193.2
-console.log(exchange(10000, currencies, 'JPY')); // -> "Валюта не знайдена"
+console.log(exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD'));
 
 
 
