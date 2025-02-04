3e831c31919848bfa495539d9efa35eea9fac258
SerhiiKI13 <kugai.sirozha@gmail.com>
2025-02-04 12:03:30 +0100

    has completed lesson 5.

--- lesson 5/main5.js
+++ lesson 5/main5.js
@@ -64,13 +64,76 @@
 
 // #bovDJDTIjt
 // - створити функцію яка приймає масив об'єктів з наступними полями id,name,age , та виводить їх в документ. Для кожного об'єкту окремий блок.
+//const arr = (users) =>{
+//     for (let i = 0; i < users.length;i++){
+//         document.write(`
+// <div>
+// <p>${users[i].id}</p>
+// <p>${users[i].name}</p>
+// <p>${users[i].age}</p>
+//  <hr>
+//  </div>
+// `);
+//     }
+// }
+// let users = [
+//     { id: 1, name: "Alice", age: 25 },
+//     { id: 2, name: "Bob", age: 30 }
+// ];
+// arr(users);
+
+
+
+
+
 // #pghbnSB
 // - створити функцію яка повертає найменьше число з масиву
+//let numbers = [10, 5, 8, 3, 15, 2, 7];
+// let minNumber = (min)=>{
+//     let num = min[0];
+//     for (let i = 1; i < min.length;i++){
+//         if (min[i] < num){
+//             num = min[i];
+//         }
+//     }
+//     return num;
+// }
+// console.log(minNumber(numbers));
+
 // #EKRNVPM
 // - створити функцію sum(arr)яка приймає масив чисел, сумує значення елементів масиву та повертає його. Приклад sum([1,2,10]) //->13
+// let sum = (arr) =>{
+//     let total = 0;
+//     for (let i = 0; i < arr.length;i++){
+//         total += arr[i];
+//     }
+//     return total;
+// }
+//
+// console.log(  sum([1,2,10]));
+
 // #kpsbSQCt2Lf
 // - створити функцію swap(arr,index1,index2). Функція міняє місцями заняення у відаовідних індексах
 // Приклад  swap([11,22,33,44],0,1) //=> [22,11,33,44]
+//let swap = (arr,index1,index2) =>{
+//     let save = arr[index1];
+//     arr[index1] = arr[index2];
+//     arr[index2] = save;
+//     return arr;
+// }
+// console.log(swap([11,22,33,44],1,1));
+
 // #mkGDenYnNjn
 // - Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 // Приклад exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD') // => 250
+
+//let exchange = (sumUAH,currencyValues,exchangeCurrency)=>{
+//     let exchangeRate = 0;
+//     for (let i = 0; i < currencyValues.length;i++){
+//         if (currencyValues[i].currency === exchangeCurrency){
+//             exchangeRate = currencyValues[i].value;
+//         }
+//     }
+//     return exchangeRate > 0 ?  sumUAH / exchangeRate : 'Валюти не знайдено';
+// }
+// console.log(exchange(10000,[{currency:'USD',value:40},{currency:'EUR',value:42}],'USD'));
